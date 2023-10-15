fbaee93eb562677c494ed4d287093e240ca0d0d7
zynx <52384539+gmZynx@users.noreply.github.com>
2023-10-15 19:06:38 +0300

    fix simple explosive

--- lua/entities/gmod_wire_simple_explosive.lua
+++ lua/entities/gmod_wire_simple_explosive.lua
@@ -64,7 +64,7 @@ function ENT:Explode()
 	local ply = self:GetPlayer()
 	if not IsValid(ply) then ply = self end
 
-	if ( ( self.Damage or 0 ) > 0 ) and ( ( CurTime() - self:GetCreationTime() ) > 3 ) then
+	if ( ( self.damage or 0 ) > 0 ) and ( ( CurTime() - self:GetCreationTime() ) > 3 ) then
 		util.BlastDamage( self, ply, self:GetPos(), self.radius, self.damage )
 	end
 
